cc33b9df2534a8c5dca7cf1e44d7b85fe0ad8684
Anna Danilenko <dissagry1347@gmail.com>
2020-09-07 15:59:38 +0300

    Add enum AccessoryKind

---
+++ FlowerPower/src/by/training/oop/flower/store/enums/AccessoryKind.java
@@ -0,0 +1,27 @@
+package by.training.oop.flower.store.enums;
+
+public enum AccessoryKind {
+
+    RIBBON(""),
+    PAPER(""),
+    POSTCARD(""),
+
+    OTHER_ACCESSORY("");
+
+    private String name;
+
+    AccessoryKind(String name) {
+        this.name = name;
+    }
+
+    public static AccessoryKind getAccessoryKind(String name) {
+        for(var item: AccessoryKind.values()){
+            if (name.equals(item.name())) {
+                return item;
+            }
+        }
+        return OTHER_ACCESSORY;
+    }
+    
+
+}
