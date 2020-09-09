6e4d7d4448d0d499fbfd5e0103464070fe215ab5
Anna Danilenko <dissagry1347@gmail.com>
2020-09-09 23:48:02 +0300

    Create enum Color

---
+++ FlowerPower/src/by/training/oop/flower/store/model/Color.java
@@ -0,0 +1,35 @@
+package by.training.oop.flower.store.model;
+
+
+
+public enum Color {
+    ORANGE(""),
+    RED(""),
+    WHITE(""),
+    YELLOW(""),
+    OTHER_COLOR("");
+
+    private String name;
+
+    Color(String name) {
+        this.setName(name);
+    }
+
+    public static Color getColor(String name) {
+        for(var item: Color.values()){
+            if (name.equals(item.name())) {
+                return item;
+            }
+        }
+        return OTHER_COLOR;}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
